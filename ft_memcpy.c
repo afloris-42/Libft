29336755562df21ecdd7551bd0ac365d0efb3aef
Aurelio Floris <babyf@Mac.station>
2024-12-20 21:56:04 +0100

    corrected

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 16:24:10 by afloris           #+#    #+#             */
-/*   Updated: 2024/12/20 21:38:36 by babyf            ###   ########.fr       */
+/*   Updated: 2024/12/20 21:55:31 by babyf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 	char *char_src;
 
 	i = 0; 
-	*char_dst = (char *)dst; 
-	*char_src = (char *)src; 
+	char_dst = (char *)dst; 
+	char_src = (char *)src; 
 	if (dst == NULL)
 		return (NULL);
 	
 	while (i < n)
 	{
-		char dst[i] = char src[i];
+		char_dst[i] = char_src[i];
 		i++;
 	}
 	return (dst);
