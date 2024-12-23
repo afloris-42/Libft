12dd3f582bd58b14809ee6cdd89a3cc54444c4ff
Aurelio Floris <afloris@e4r12p1.42roma.it>
2024-12-23 16:42:01 +0100

    + itoa, check edge cases

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,13 +6,14 @@
 /*   By: babyf <babyf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 13:52:08 by afloris           #+#    #+#             */
-/*   Updated: 2024/12/23 14:46:39 by babyf            ###   ########.fr       */
+/*   Updated: 2024/12/23 16:40:04 by afloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-int	count_digit(char *s, int n)
+int	count_digit(int n)
 {
 	int	count;
 
@@ -20,7 +21,7 @@ int	count_digit(char *s, int n)
 		return (1);
 	
 	count = 0; 
-	while (n < 0)
+	if (n < 0)
 	{
 		n = -n;
 		count++;
@@ -33,33 +34,36 @@ int	count_digit(char *s, int n)
 	return (count);
 }
 
-int	ft_isneg(int n)
-{
-	if (n < 0) 
-	{
-        n = -n;
-    }
-	return (n);
-}
+
 
 char	*ft_itoa(int n)
 {
-	int	nb_digit; 
-	int	neg; 
+	int	nb_digit;
 	char	*s;
 
-	nb_digit = count_digit (n); //can you?? 
-	neg = ft_isneg;
-	s = malloc(nb_digit + 1);
+	nb_digit = count_digit (n);
+	s = (char *)malloc((nb_digit + 1) * sizeof(char));
 
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = -n;
+	}
 	if (!s)
 		return (NULL);
-	if (n == 0)
+	s[nb_digit] = '\0';
+	while (n > 0)
 	{
-		s[0] = '0'; 
-		s[1] = '\0';
-		return (s);
+		s[--nb_digit] = (n % 10) + '0';
+		n /= 10;
 	}
-	//if int is negative
+	return (s);
+}
 
+int	main (void)
+{
+	int	i = 2147483647;
+
+	printf ("S is: %s", ft_itoa(i));
+	return (0);
 }
