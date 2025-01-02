2821610959f086209a7906861cb07650b3c272a0
Aurelio Floris <babyf@MacBook-Air-di-Auri.local>
2025-01-02 17:21:10 +0100

    fix

--- libft.h
+++ libft.h
@@ -23,55 +23,19 @@ typedef struct	s_list
 {
 	void		*content;
 	struct s_list	*next;
-<<<<<<< HEAD
-}					t_list;
-
-int	ft_atoi(const char *str);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t count, size_t size);
-=======
 }			t_list;
 
 int	ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
-void            *ft_calloc(size_t count, size_t size);
->>>>>>> 3f3d5f3205406945b5236c7782c679729e830f7f
+void	*ft_calloc(size_t count, size_t size);
 int	ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
 int	ft_isdigit(int c);
 int	ft_isprint(int c);
-<<<<<<< HEAD
 char	*ft_itoa(int n);
-void ft_lstadd_back(t_list **lst, t_list *new);
-void ft_lstadd_front(t_list **lst, t_list *new);
-=======
-int	ft_tolower(int c);
-int	ft_toupper(int c);
-int	ft_memcmp(const void *s1, const void *s2, size_t n);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-void		ft_putchar_fd(char c, int fd);
-void		ft_putendl_fd(char *s, int fd);
-void		ft_putstr_fd(char *s, int fd);
-void		ft_putnbr_fd(int n, int fd);
-void		*ft_memchr(const void *s, int c, size_t n);
-void		*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-void		*ft_memmove(void *dst, const void *src, size_t len);
-void		*ft_memset(void *b, int c, size_t len);
-void		ft_striteri(char * s, void ( * f)(unsigned int, char *));
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char		*ft_substr(char const *s, unsigned int start,size_t len);
-char		*strdup(const char *s1);
-char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
-char		*ft_strrchr(const char *s, int c);
-char		**ft_split(char const *s, char c);
-char		*ft_strtrim(char const *s1, char const *set);
-size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize);
-size_t	ft_strlen(const char *s);
 void    ft_lstadd_back(t_list **lst, t_list *new);
 void    ft_lstadd_front(t_list **lst, t_list *new);
->>>>>>> 3f3d5f3205406945b5236c7782c679729e830f7f
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lsiter(t_list *lst, void (*f)(void *f));
